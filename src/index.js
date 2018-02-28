3950bc865b5ad0a27e83c36337a7a893fbc691ee
Sviatlana Kuraptsova <kuraptsova.s@gmail.com>
2018-02-28 08:38:54 +0100

    [zeros]/feature: added solution: counts the quantity of multipliers - 5 to count zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let getNun = function(value, multiplier) {
+        let result = (value - value%multiplier)/multiplier;
+        if (result >= multiplier) {
+            result += getNun(result, multiplier);
+        }
+        return result;
+    };
+    return getNun(number, 5);
 }
+
